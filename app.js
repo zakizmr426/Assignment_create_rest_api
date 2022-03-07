d2e167c0fe8a1fdcd28adca26362e7d8cd6350bb
Zaki <zakizmr426@gmail.com>
2022-03-08 00:19:33 +0600

    Second Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// const dishRouter = require(__dirname + '/dishRouter');
+// const promoRouter = require(__dirname + '/promoRouter');
+// const leaderRouter = require(__dirname + '/leaderRouter');
+ 
+// app.use('/dishes', dishRouter);
+// app.use('/promotions', promoRouter);
+// app.use('/leaders', leaderRouter);
+ 
+app.get('/', (req, res) => {
+  res.send('Welcome to the project')
+})
+
+app.listen(port, () => {
+  console.log(`Server Started on ${port}`)
+})
