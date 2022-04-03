dabec0f537da504bf260732747e46392ed2556a2
Zaki <zakizmr426@gmail.com>
2022-04-04 01:05:31 +0600

    2nd assignment completed

--- leaderRouter.js
+++ leaderRouter.js
@@ -1,56 +1,171 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
- 
-const dishRouter = express.Router();
- 
-dishRouter.use(bodyParser.json());
- 
-dishRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res,next) => {
-    res.end('All the leaders will be send');
-})
-.post((req, res, next) => {
-    res.end('POST method called and will be processed accordingly');
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /leaders');
-})
-.patch((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PATCH operation not supported on /leaders');
-})
-.delete((req, res, next) => {
-    res.end('Deleting all the leaders');
-});
+const leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+leaderRouter.use(bodyParser.json({ type: 'application/*+json' }))
 
-dishRouter.route('/:leaderId')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res,next) => {
-    res.end('Will send the leader  with id ' +req.params.leaderId);
-})
-.post((req, res, next) => {
-    res.end('POST operation not supported on /leaders/:' +req.params.leaderId);
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Will Update the leader with id ' +req.params.leaderId);
-})
-.patch((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Will Update the leader with id ' +req.params.leaderId);
-})
-.delete((req, res, next) => {
-    res.end('Deleting the leader with id ' +req.params.leaderId);
+
+const leadersSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    designation: String,
+    abbr: String,
+    description: String,
+    featured: Boolean
 });
- 
-module.exports = dishRouter;
+const Leader = mongoose.model('Leader', leadersSchema);
+
+leaderRouter.route('/')
+    .get((req, res, next) => {
+        Leader.find({}, function (err, foundItems) {
+            if (!err) {
+                var count = foundItems.length;
+                if (count === 0) {
+                    res.send("There are no Leaders in the database");
+                }
+                else {
+                    res.end('Found Leaders in the database, and they are: \n' + foundItems);
+                }
+
+            }
+
+            else {
+                res.end("Could not access the database");
+            }
+        });
+    })
+
+
+    .post((req, res, next) => {
+        //Correct input format with feautred true
+        if (req.body.name && req.body.image && req.body.designation && req.body.abbr && req.body.description && req.body.featured) {
+            const leader = new Leader({
+                name: req.body.name,
+                image: req.body.image,
+                designation: req.body.designation,
+                abbr: req.body.abbr,
+                description: req.body.description,
+                featured: true
+            });
+
+            leader.save(function (err, result) {
+                if (err) {
+                    res.end("Some error occured and could not save the data");
+                }
+                else {
+                    res.end("Successfully saved the data");
+                }
+            })
+        }
+
+        //Correct input format but featured is false
+        else if (req.body.name && req.body.image && req.body.designation && req.body.abbr && req.body.description){
+            const lead = new Leader({
+                name: req.body.name,
+                image: req.body.image,
+                designation: req.body.designation,
+                abbr: req.body.abbr,
+                description: req.body.description,
+                featured: false
+            });
+
+            lead.save(function (err, result) {
+                if (err) {
+                    res.end("Some error occured and could not save the data");
+                }
+                else {
+                    res.end("Successfully saved the data");
+                }
+            })
+
+        }
+
+        //Wrong input fomat
+        else {
+            res.end("Wrong input form");
+        }
+    })
+
+
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /leaders');
+    })
+
+
+    .patch((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PATCH operation not supported on /leaders');
+    })
+
+    .delete((req, res, next) => {
+        Leader.deleteMany({}, function(err){
+            if(!err)
+            {
+                res.end("Deleted all the leaders from the database");
+            }
+            else
+            {
+                res.end("Some error occured and could not delete")
+            }
+        })
+    });
+
+leaderRouter.route('/:leaderId')
+    /*.all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })*/
+
+    .get((req, res, next) => {
+        var id = new mongoose.Types.ObjectId(req.params.promoId);
+        Leader.findById(id, function (err, foundItems) {
+            if (!err) {
+                if(foundItems)
+                {
+                    res.end('Found Items in the database with this id , and they are: \n' + foundItems); 
+                }
+                else
+                {
+                    res.end("There are no items with id " +id)
+                }
+                               
+            }
+            else {
+                res.end("Could not access the database");
+            }
+        });
+    })
+
+    .post((req, res, next) => {
+        res.end('POST operation not supported on /leaders/:' + req.params.leaderId);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Will Update the leader with id ' + req.params.leaderId);
+    })
+    .patch((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Will Update the leader with id ' + req.params.leaderId);
+    })
+    .delete((req, res, next) => {
+        var id = new mongoose.Types.ObjectId(req.params.promoId);
+        Promotion.deleteOne({_id: id}, function(err, deletedPrmotions){
+            if(!err)
+            {
+                if(deletedPrmotions. deletedCount === 1)
+                {
+                    res.end('Deleting the promotion of the employee with id ' + id);
+                }
+                else
+                {
+                    res.send("No item to delete with this id");
+                }
+                
+            }
+        });
+    });
+
+module.exports = leaderRouter;
--- promoRouter.js
+++ promoRouter.js
@@ -1,56 +1,171 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
- 
-const dishRouter = express.Router();
- 
-dishRouter.use(bodyParser.json());
- 
-dishRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res,next) => {
-    res.end('All the employes will be promoted');
-})
-.post((req, res, next) => {
-    res.end('POST method called and will be processed accordingly');
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /promotions');
-})
-.patch((req, res, next) => {
-    res.statusCode = 403;
-    res.end('PATCH operation not supported on /promotions');
-})
-.delete((req, res, next) => {
-    res.end('Deleting all the promotions');
+mongoose.connect('mongodb://localhost:27017/assignmentdb');
+const promoRouter = express.Router();
+promoRouter.use(bodyParser.json());
+promoRouter.use(bodyParser.json({ type: 'application/*+json' }))
+
+const promotionsSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    label: String,
+    price: Number,
+    description: String,
+    featured: Boolean
 });
 
-dishRouter.route('/:promoId')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res,next) => {
-    res.end('Will promote the employee with id ' +req.params.promoId);
-})
-.post((req, res, next) => {
-    res.end('POST operation not supported on /promotions/:' +req.params.promoId);
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Will Update the employee with id ' +req.params.promoId);
-})
-.patch((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Will Update the employee with id ' +req.params.promoId);
-})
-.delete((req, res, next) => {
-    res.end('Deleting the promotion of the employee with id ' +req.params.promoId);
-});
- 
-module.exports = dishRouter;
+const Promotion = mongoose.model('Promotion', promotionsSchema);
+
+promoRouter.route('/')
+    .get((req, res, next) => {
+        Promotion.find({}, function (err, foundItems) {
+            if (!err) {
+                var count = foundItems.length;
+                if(count === 0)
+                {
+                    res.send("There are no pormotions in the database");
+                }
+                else
+                {
+                    res.end('Found Items in the database, and they are: \n' + foundItems);
+                }
+                
+            }
+
+            else {
+                res.end("Could not access the database");
+            }
+        });
+
+    })
+
+    .post((req, res, next) => {
+       
+        //Correct input format with feautred true
+        if (req.body.name && req.body.image && req.body.label && req.body.price && req.body.description && req.body.featured) {
+            const promo = new Promotion({
+                name: req.body.name,
+                image: req.body.image,
+                label: req.body.label,
+                price: req.body.price,
+                description: req.body.description,
+                featured: true
+            });
+
+            promo.save(function(err, result){
+                if(err)
+                {
+                    res.send("Some error occured and could not save the data");
+                }
+                else
+                {
+                    res.send("Successfully saved the data");
+                }
+            })
+        }   
+
+        //Correct input format but featured is false
+        else if(req.body.name && req.body.image && req.body.label && req.body.price && req.body.description) {
+            const promo = new Promotion({
+                name: req.body.name,
+                image: req.body.image,
+                label: req.body.label,
+                price: req.body.price,
+                description: req.body.description,
+                featured: false
+            });
+            
+            promo.save(function(err, result){
+                if(err)
+                {
+                    console.log("Inside else if\n");
+                    res.send("Some error occured and could not save the data");
+                }
+                else
+                {
+                    res.send("Successfully saved the data");
+                }
+            })
+
+        }
+
+        //Wrong input fomat
+        else {
+            res.send("Wrong input form");
+        }
+
+    })
+
+
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /promotions');
+    })
+    .patch((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PATCH operation not supported on /promotions');
+    })
+    
+    .delete((req, res, next) => {
+        Promotion.deleteMany({}, function(err){
+            if(!err)
+            {
+                res.end("Deleted all the elements from the database");
+            }
+        })
+    });
+
+promoRouter.route('/:promoId')    
+    .get((req, res, next) => {
+        var id = new mongoose.Types.ObjectId(req.params.promoId);
+        Promotion.findById(id, function (err, foundItems) {
+            if (!err) {
+                if(foundItems)
+                {
+                    res.end('Found Items in the database with this id , and they are: \n' + foundItems); 
+                }
+                else
+                {
+                    res.end("There are no items with id " +id)
+                }
+                               
+            }
+            else {
+                res.end("Could not access the database");
+            }
+        });
+    })
+    
+    .post((req, res, next) => {
+        res.end('POST operation not supported on /promotions/:' + req.params.promoId);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Will Update the employee with id ' + req.params.promoId);
+    })
+    .patch((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Will Update the employee with id ' + req.params.promoId);
+    })
+   
+    .delete((req, res, next) => {
+        var id = new mongoose.Types.ObjectId(req.params.promoId);
+        Promotion.deleteOne({_id: id}, function(err, deletedPrmotions){
+            if(!err)
+            {
+                if(deletedPrmotions. deletedCount === 1)
+                {
+                    res.end('Deleting the promotion of the employee with id ' + id);
+                }
+                else
+                {
+                    res.send("No item to delete with this id");
+                }
+                
+            }
+        });
+       
+    });
+
+module.exports = promoRouter;
